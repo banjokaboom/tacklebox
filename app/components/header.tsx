4a9fa85362887baa63031b1b5d241611086c11fc
Andrew Augusto <augustoandrew@gmail.com>
2023-06-19 23:25:04 -0400

    slight tweak to global padding

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ import { faFishFins } from '@fortawesome/free-solid-svg-icons'
 
 export default function header() {
   return (
-    <header className="flex flex-col items-center justify-between p-24 pt-12 pb-12 bg-slate-50 mx-auto text-slate-700">
+    <header className="flex flex-col items-center justify-between p-12 lg:p-24 lg:pt-12 lg:pb-12 bg-slate-50 mx-auto text-slate-700">
       <div className="max-w-5xl w-full">
         <div className="flex flex-row">
           <a href="/" className="text-3xl">
