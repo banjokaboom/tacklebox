48063941f00bad7b0deff06a189ae77ef9843814
Andrew Augusto <augustoandrew@gmail.com>
2023-06-19 22:46:35 -0400

    fixed homepage format, using state and binding for what to fish app with zip code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between">
-      <h1 className="text-3xl pb-4">Home</h1>
-      <hr />
+      <div className="max-w-5xl w-full">
+        <h1 className="text-3xl pb-4">Home</h1>
+        <hr />
+      </div>
     </div>
   )
 }
--- app/pages/what-to-fish/page.tsx
+++ app/pages/what-to-fish/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 
 import * as tackleList from './tackle.js'
-import { useState } from 'react'
-
-const waterTempMultiplier = 0.87
+import { useState, useEffect } from 'react'
 
 class FishingData {
   public baitRecommendations: BaitRecommendations
   public seasons: string
   public tackle: object[]
   public weather: WeatherData
+
+  constructor() {
+    this.baitRecommendations = new BaitRecommendations()
+    this.seasons = ''
+    this.tackle = []
+    this.weather = new WeatherData()
+  }
 }
 
 class WeatherData {
@@ -17,279 +22,333 @@ class WeatherData {
   public waterTemp: string
   public conditions: string
   public wind: string
-  public current: WeatherData
-  public forecast: WeatherData
+  public current: WeatherDataChild
+  public forecast: WeatherDataChild
+
+  constructor() {
+    this.outdoorTemp = ''
+    this.waterTemp = ''
+    this.conditions = ''
+    this.wind = ''
+    this.current = new WeatherDataChild()
+    this.forecast = new WeatherDataChild()
+  }
+}
+
+class WeatherDataChild {
+  public outdoorTemp: string
+  public waterTemp: string
+  public conditions: string
+  public wind: string
+
+  constructor() {
+    this.outdoorTemp = ''
+    this.waterTemp = ''
+    this.conditions = ''
+    this.wind = ''
+  }
 }
 
 class BaitRecommendations {
   public colorsToUse: string
   public baitsToUse: string
-}
 
-async function getData() {
-  let data = new FishingData()
-
-  const weather = await getWeather()
-
-  console.log('Weather received.')
-  console.log(weather)
-
-  data.baitRecommendations = pickBaitRecommendations(weather)
-  data.seasons = getSeasons()
-  data.tackle = await pickTackle(weather)
-  data.weather = getWeatherValues(weather)
-
-  return data
-}
-
-async function getWeather() {
-  const res = await fetch(
-    'http://api.weatherapi.com/v1/forecast.json?key=fbbd41244a6947eb83c182430231306&q=01516',
-    { cache: 'no-store' }
-  )
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
+  constructor() {
+    this.colorsToUse = ''
+    this.baitsToUse = ''
   }
-
-  return res.json()
 }
 
-async function pickTackle(weather: WeatherData): Promise<object[]> {
-  console.log('Tackle loaded.')
-  console.log(JSON.stringify(tackleList))
+export default function WhatToFish() {
+  /**
+   * Render running infinitely
+   */
+  let [zip, setZip] = useState('01516')
+  let [data, setData] = useState(new FishingData())
 
-  let tackleToUse: object[] = []
+  useEffect(() => {
+    const waterTempMultiplier = 0.87
 
-  tackleList.forEach(function (tackle) {
-    console.log(tackle)
+    async function getWeather(zip: string) {
+      if (!zip || zip.length !== 5) {
+        return
+      }
+      const res = await fetch(
+        'http://api.weatherapi.com/v1/forecast.json?key=fbbd41244a6947eb83c182430231306&q=' +
+          zip,
+        { cache: 'no-store' }
+      )
 
-    if (isTackleForWeather(tackle, weather)) {
-      tackleToUse.push(tackle)
+      if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+      }
+
+      return res.json()
     }
-  })
 
-  return tackleToUse
-}
+    async function pickTackle(weather: WeatherData): Promise<object[]> {
+      console.log('Tackle loaded.')
 
-function pickBaitRecommendations(weather: any): BaitRecommendations {
-  const seasons = getSeasons()
-  let baitRecommendations = new BaitRecommendations()
-  let colorsToUse: string[] = []
-  let baitToUse: string[] = []
+      let tackleToUse: object[] = []
 
-  baitToUse.push('live worms, soft plastic worms')
+      tackleList.forEach(function (tackle: object) {
+        if (isTackleForWeather(tackle, weather)) {
+          tackleToUse.push(tackle)
+        }
+      })
 
-  if (seasons.includes('spring')) {
-    colorsToUse.push('craw', 'orange', 'red')
-    baitToUse.push('powerbait, soft plastic craws')
-  } else {
-    colorsToUse.push('shad', 'baitfish', 'white', 'blue')
-    baitToUse.push(
-      'soft plastic swimbaits, soft plastic flukes, soft plastic fish'
-    )
-
-    if (seasons.includes('summer')) {
-      baitToUse.push('soft plastic frogs, soft plastic lizards')
-    } else {
-      baitToUse.push('powerbait')
+      return tackleToUse
     }
-  }
 
-  if (weather.current.cloud >= 75) {
-    colorsToUse.push('bright', 'yellow', 'pink')
-  } else {
-    colorsToUse.push('natural', 'gold', 'silver', 'green')
-  }
+    function pickBaitRecommendations(weather: any): BaitRecommendations {
+      const seasons = getSeasons()
+      let baitRecommendations = new BaitRecommendations()
+      let colorsToUse: string[] = []
+      let baitToUse: string[] = []
 
-  let colorString = ''
+      baitToUse.push('live worms, soft plastic worms')
 
-  colorsToUse.forEach(function (color, index) {
-    colorString += color
-    if (index < colorsToUse.length - 1) {
-      colorString += ', '
+      if (seasons.includes('spring')) {
+        colorsToUse.push('craw', 'orange', 'red')
+        baitToUse.push('powerbait, soft plastic craws')
+      } else {
+        colorsToUse.push('shad', 'baitfish', 'white', 'blue')
+        baitToUse.push(
+          'soft plastic swimbaits, soft plastic flukes, soft plastic fish'
+        )
+
+        if (seasons.includes('summer')) {
+          baitToUse.push('soft plastic frogs, soft plastic lizards')
+        } else {
+          baitToUse.push('powerbait')
+        }
+      }
+
+      if (weather.current.cloud >= 75) {
+        colorsToUse.push('bright', 'yellow', 'pink')
+      } else {
+        colorsToUse.push('natural', 'gold', 'silver', 'green')
+      }
+
+      let colorString = ''
+
+      colorsToUse.forEach(function (color, index) {
+        colorString += color
+        if (index < colorsToUse.length - 1) {
+          colorString += ', '
+        }
+      })
+
+      let baitString = ''
+
+      baitToUse.forEach(function (color, index) {
+        baitString += color
+        if (index < baitToUse.length - 1) {
+          baitString += ', '
+        }
+      })
+
+      baitRecommendations.colorsToUse = colorString
+      baitRecommendations.baitsToUse = baitString
+
+      return baitRecommendations
     }
-  })
 
-  let baitString = ''
+    function isTackleForWeather(tackle: any, weather: any): boolean {
+      const seasons = getSeasons()
+      let warmWaterMax = 75
+      let warmWaterMin = 55
+      let waterTemp =
+        weather.forecast.forecastday[0].day.maxtemp_f * waterTempMultiplier
 
-  baitToUse.forEach(function (color, index) {
-    baitString += color
-    if (index < baitToUse.length - 1) {
-      baitString += ', '
-    }
-  })
+      if (
+        seasons.includes('bass pre-spawn') ||
+        seasons.includes('bass spawn') ||
+        seasons.includes('fall')
+      ) {
+        console.log(
+          "It's growing season for bass! Bring out the reaction baits!"
+        )
+        if (tackle.speed.includes('fast') || tackle.depth.includes('shallow')) {
+          return true
+        }
+      }
 
-  baitRecommendations.colorsToUse = colorString
-  baitRecommendations.baitsToUse = baitString
+      if (waterTemp >= warmWaterMax) {
+        console.log('Water is very warm, need to fish deep and slow!')
 
-  return baitRecommendations
-}
+        if (!tackle.waterTemp.includes('warm')) {
+          return false
+        }
 
-function isTackleForWeather(tackle: any, weather: any): boolean {
-  const seasons = getSeasons()
-  let warmWaterMax = 75
-  let warmWaterMin = 55
-  let waterTemp =
-    weather.forecast.forecastday[0].day.maxtemp_f * waterTempMultiplier
+        if (!tackle.depth.includes('deep')) {
+          return false
+        }
+      } else if (waterTemp >= warmWaterMin) {
+        console.log('Water temp is ideal for fishing most lures and rigs!')
 
-  if (
-    seasons.includes('bass pre-spawn') ||
-    seasons.includes('bass spawn') ||
-    seasons.includes('fall')
-  ) {
-    console.log("It's growing season for bass! Bring out the reaction baits!")
-    if (tackle.speed.includes('fast') || tackle.depth.includes('shallow')) {
+        if (!tackle.waterTemp.includes('warm')) {
+          return false
+        }
+
+        if (!tackle.depth.includes('shallow')) {
+          return false
+        }
+      } else {
+        console.log('Water temp is cold! At least for bass and most panfish.')
+
+        if (!tackle.waterTemp.includes('cold')) {
+          return false
+        }
+
+        if (!tackle.speed.includes('still') && !tackle.speed.includes('slow')) {
+          return false
+        }
+
+        if (!tackle.depth.includes('deep')) {
+          return false
+        }
+      }
+
       return true
     }
-  }
 
-  if (waterTemp >= warmWaterMax) {
-    console.log('Water is very warm, need to fish deep and slow!')
+    function getSeasons(): string {
+      let today = new Date()
+      let todayMonth = today.getMonth() + 1
+      let seasons: string[] = []
 
-    if (!tackle.waterTemp.includes('warm')) {
-      return false
+      switch (todayMonth) {
+        case 1:
+          seasons.push('winter')
+          break
+        case 2:
+          seasons.push('winter')
+          break
+        case 3:
+          seasons.push(today.getDate() > 21 ? 'spring' : 'winter')
+          break
+        case 4:
+          seasons.push('spring')
+          break
+        case 5:
+          seasons.push('spring')
+          seasons.push('bass pre-spawn')
+          break
+        case 6:
+          seasons.push(today.getDate() > 21 ? 'summer' : 'spring')
+          seasons.push('bass spawn')
+          seasons.push('sunfish pre-spawn')
+          break
+        case 7:
+          seasons.push('summer')
+          seasons.push('sunfish spawn')
+          break
+        case 8:
+          seasons.push('summer')
+          break
+        case 9:
+          seasons.push(today.getDate() > 21 ? 'fall' : 'summer')
+          break
+        case 10:
+          seasons.push('fall')
+          break
+        case 11:
+          seasons.push('fall')
+          break
+        case 12:
+          seasons.push(today.getDate() > 21 ? 'winter' : 'fall')
+          break
+        default:
+          break
+      }
+
+      let seasonString = ''
+
+      seasons.forEach(function (season, index) {
+        seasonString += season
+        if (index < seasons.length - 1) {
+          seasonString += ', '
+        }
+      })
+
+      return seasonString
     }
 
-    if (!tackle.depth.includes('deep')) {
-      return false
+    function getWeatherValues(weather): WeatherData {
+      let weatherData = new WeatherData()
+      let current = new WeatherDataChild()
+      let forecast = new WeatherDataChild()
+
+      current.outdoorTemp = weather.current.temp_f + 'F'
+      current.waterTemp =
+        (weather.current.feelslike_f * waterTempMultiplier).toFixed(0) + 'F'
+      current.conditions = weather.current.condition.text
+      current.wind = weather.current.wind_mph + 'mph'
+
+      forecast.outdoorTemp = weather.forecast.forecastday[0].day.maxtemp_f + 'F'
+      forecast.waterTemp =
+        (
+          weather.forecast.forecastday[0].day.maxtemp_f * waterTempMultiplier
+        ).toFixed(0) + 'F'
+      forecast.conditions = weather.forecast.forecastday[0].day.condition.text
+      forecast.wind = weather.forecast.forecastday[0].day.maxwind_mph + 'mph'
+
+      weatherData.current = current
+      weatherData.forecast = forecast
+
+      return weatherData
     }
-  } else if (waterTemp >= warmWaterMin) {
-    console.log('Water temp is ideal for fishing most lures and rigs!')
 
-    if (!tackle.waterTemp.includes('warm')) {
-      return false
+    async function getData(zip: string) {
+      let fishingData = new FishingData()
+
+      const weather = await getWeather(zip)
+
+      if (weather) {
+        console.log('Weather received.')
+
+        fishingData.baitRecommendations = pickBaitRecommendations(weather)
+        fishingData.seasons = getSeasons()
+        fishingData.tackle = await pickTackle(weather)
+        fishingData.weather = getWeatherValues(weather)
+
+        if (!isDataLoaded) {
+          setData(fishingData)
+        }
+      }
     }
 
-    if (!tackle.depth.includes('shallow')) {
-      return false
+    let isDataLoaded = false
+
+    getData(zip)
+
+    return () => {
+      isDataLoaded = true
     }
-  } else {
-    console.log('Water temp is cold! At least for bass and most panfish.')
-
-    if (!tackle.waterTemp.includes('cold')) {
-      return false
-    }
-
-    if (!tackle.speed.includes('still') && !tackle.speed.includes('slow')) {
-      return false
-    }
-
-    if (!tackle.depth.includes('deep')) {
-      return false
-    }
-  }
-
-  return true
-}
-
-function getSeasons(): string {
-  let today = new Date()
-  let todayMonth = today.getMonth() + 1
-  let seasons: string[] = []
-
-  switch (todayMonth) {
-    case 1:
-      seasons.push('winter')
-      break
-    case 2:
-      seasons.push('winter')
-      break
-    case 3:
-      seasons.push(today.getDate() > 21 ? 'spring' : 'winter')
-      break
-    case 4:
-      seasons.push('spring')
-      break
-    case 5:
-      seasons.push('spring')
-      seasons.push('bass pre-spawn')
-      break
-    case 6:
-      seasons.push(today.getDate() > 21 ? 'summer' : 'spring')
-      seasons.push('bass spawn')
-      seasons.push('sunfish pre-spawn')
-      break
-    case 7:
-      seasons.push('summer')
-      seasons.push('sunfish spawn')
-      break
-    case 8:
-      seasons.push('summer')
-      break
-    case 9:
-      seasons.push(today.getDate() > 21 ? 'fall' : 'summer')
-      break
-    case 10:
-      seasons.push('fall')
-      break
-    case 11:
-      seasons.push('fall')
-      break
-    case 12:
-      seasons.push(today.getDate() > 21 ? 'winter' : 'fall')
-      break
-    default:
-      break
-  }
-
-  let seasonString = ''
-
-  seasons.forEach(function (season, index) {
-    seasonString += season
-    if (index < seasons.length - 1) {
-      seasonString += ', '
-    }
-  })
-
-  return seasonString
-}
-
-function getWeatherValues(weather): WeatherData {
-  let data = new WeatherData()
-  let current = new WeatherData()
-  let forecast = new WeatherData()
-
-  current.outdoorTemp = weather.current.temp_f + 'F'
-  current.waterTemp =
-    (weather.current.feelslike_f * waterTempMultiplier).toFixed(0) + 'F'
-  current.conditions = weather.current.condition.text
-  current.wind = weather.current.wind_mph + 'mph'
-
-  forecast.outdoorTemp = weather.forecast.forecastday[0].day.maxtemp_f + 'F'
-  forecast.waterTemp =
-    (
-      weather.forecast.forecastday[0].day.maxtemp_f * waterTempMultiplier
-    ).toFixed(0) + 'F'
-  forecast.conditions = weather.forecast.forecastday[0].day.condition.text
-  forecast.wind = weather.forecast.forecastday[0].day.maxwind_mph + 'mph'
-
-  data.current = current
-  data.forecast = forecast
-
-  return data
-}
-
-export default async function WhatToFish() {
-  const [zip, setZip] = useState('01516')
-  const data = await getData()
+  }, [zip])
 
   return (
     <div className="flex flex-col items-center justify-between">
       <div className="max-w-5xl w-full">
         <h1 className="text-3xl pb-4">What to Fish</h1>
         <hr className="pb-4" />
-        <div className="pb-4">
-          <label htmlFor="zip">ZIP Code</label>
+        <div className="pb-4 flex-col lg:flex-row justify-between">
+          <label htmlFor="zip" className="pb-4 lg:pb-0">
+            ZIP Code
+          </label>
           <input
             type="text"
             name="zip"
             id="zip"
-            onChange={setZip}
-            className="text-slate-700 ml-4"
+            onChange={(e) => {
+              setZip(e.target.value)
+            }}
+            className="text-slate-700 lg:ml-4 leading-4 p-2"
           />
         </div>
+
         <div className="flex flex-col lg:flex-row justify-between">
           <div>
             <h2 className="text-2xl pb-8 pt-8">
