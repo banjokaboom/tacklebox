12d3a2d7a846322b1f3bf55cd028c921905f216a
Andrew Augusto <augustoandrew@gmail.com>
2023-06-25 13:22:40 -0400

    updated what to fish test for more coverage

--- __tests__/app/pages/can-i-fish/page.test.js
+++ __tests__/app/pages/can-i-fish/page.test.js
@@ -35,6 +35,27 @@ afterEach(() => {
 
 afterAll(() => server.close())
 
+function resetFishingData() {
+  fwFishingData = [
+    {
+      species: 'Black Bass',
+      description: 'Black Bass (Largemouth and Smallmouth, in any combination)',
+      seasonDates: ['Jan 1 – Jun 30', 'Jul 1 -  Oct 31', 'Nov 1 - Dec 31'],
+      seasonLimits: [' 5', ' '],
+      minimumLength: ', 12, ',
+    },
+  ]
+  swFishingData = [
+    {
+      species: 'Black Sea Bass',
+      description: 'Black Sea Bass',
+      seasonDates: ['May 20 - Sept 7'],
+      seasonLimits: ['4 fish'],
+      minimumLength: 'Min: 16.5"',
+    },
+  ]
+}
+
 describe('CanIFish', () => {
   it('renders a heading', () => {
     render(<CanIFish />)
@@ -65,24 +86,3 @@ describe('CanIFish', () => {
     expect(noText).toBeInTheDocument()
   })
 })
-
-function resetFishingData() {
-  fwFishingData = [
-    {
-      species: 'Black Bass',
-      description: 'Black Bass (Largemouth and Smallmouth, in any combination)',
-      seasonDates: ['Jan 1 – Jun 30', 'Jul 1 -  Oct 31', 'Nov 1 - Dec 31'],
-      seasonLimits: [' 5', ' '],
-      minimumLength: ', 12, ',
-    },
-  ]
-  swFishingData = [
-    {
-      species: 'Black Sea Bass',
-      description: 'Black Sea Bass',
-      seasonDates: ['May 20 - Sept 7'],
-      seasonLimits: ['4 fish'],
-      minimumLength: 'Min: 16.5"',
-    },
-  ]
-}
--- __tests__/app/pages/what-to-fish/page.test.js
+++ __tests__/app/pages/what-to-fish/page.test.js
@@ -1,6 +1,9 @@
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
-import { render, screen } from '@testing-library/react'
+import {
+  render,
+  screen,
+} from '@testing-library/react'
 import {
   describe,
   it,
@@ -21,83 +24,9 @@ jest.mock('../../../../app/pages/what-to-fish/cityStates.js', () => [], {
   virtual: true,
 })
 
-/* cSpell:disable */
-let weatherData = {
-  location: {
-    name: 'Boston',
-    region: 'Massachusetts',
-    country: 'United States of America',
-    lat: 42.36,
-    lon: -71.06,
-    tz_id: 'America/New_York',
-    localtime_epoch: 1687491648,
-    localtime: '2023-06-22 23:40',
-  },
-  current: {
-    last_updated_epoch: 1687491000,
-    last_updated: '2023-06-22 23:30',
-    temp_c: 16.7,
-    temp_f: 62.1,
-    is_day: 0,
-    condition: {
-      text: 'Partly cloudy',
-      icon: '//cdn.weatherapi.com/weather/64x64/night/116.png',
-      code: 1003,
-    },
-    wind_mph: 6.9,
-    wind_kph: 11.2,
-    wind_degree: 50,
-    wind_dir: 'NE',
-    pressure_mb: 1020.0,
-    pressure_in: 30.13,
-    precip_mm: 0.0,
-    precip_in: 0.0,
-    humidity: 80,
-    cloud: 75,
-    feelslike_c: 16.7,
-    feelslike_f: 62.1,
-    vis_km: 16.0,
-    vis_miles: 9.0,
-    uv: 1.0,
-    gust_mph: 7.6,
-    gust_kph: 12.2,
-  },
-  forecast: {
-    forecastday: [
-      {
-        date: '2023-06-22',
-        date_epoch: 1687392000,
-        day: {
-          maxtemp_c: 19.5,
-          maxtemp_f: 67.1,
-          mintemp_c: 11.9,
-          mintemp_f: 53.4,
-          avgtemp_c: 15.5,
-          avgtemp_f: 59.9,
-          maxwind_mph: 8.9,
-          maxwind_kph: 14.4,
-          totalprecip_mm: 0.0,
-          totalprecip_in: 0.0,
-          totalsnow_cm: 0.0,
-          avgvis_km: 6.9,
-          avgvis_miles: 4.0,
-          avghumidity: 90.0,
-          daily_will_it_rain: 0,
-          daily_chance_of_rain: 0,
-          daily_will_it_snow: 0,
-          daily_chance_of_snow: 0,
-          condition: {
-            text: 'Mist',
-            icon: '//cdn.weatherapi.com/weather/64x64/day/143.png',
-            code: 1030,
-          },
-          uv: 4.0,
-        },
-      },
-    ],
-  },
-}
-/* cSpell:enable */
+
+let weatherData = {}
+let date = new Date()
 
 const server = setupServer(
   rest.get('http://api.weatherapi.com/v1/forecast.json', (req, res, ctx) => {
@@ -105,12 +34,133 @@ const server = setupServer(
   })
 )
 
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
+beforeAll(() => {
+  resetTestData()
+  server.listen()
+})
+afterEach(() => {
+  resetTestData()
+  server.resetHandlers()
+})
 afterAll(() => server.close())
+
+function resetTestData() {
+  date = new Date()
+
+  /* cSpell:disable */
+  weatherData = {
+    location: {
+      name: 'Boston',
+      region: 'Massachusetts',
+    },
+    current: {
+      temp_f: 75,
+      condition: {
+        text: 'Partly cloudy',
+        icon: '//cdn.weatherapi.com/weather/64x64/night/116.png',
+        code: 1003,
+      },
+      wind_mph: 6.9,
+      cloud: 0,
+      feelslike_f: 75,
+    },
+    forecast: {
+      forecastday: [
+        {
+          day: {
+            maxtemp_f: 75,
+            mintemp_f: 60,
+            avgtemp_f: 62.5,
+            maxwind_mph: 8.9,
+            condition: {
+              text: 'Mist',
+            },
+          },
+        },
+      ],
+    },
+  }
+  /* cSpell:enable */
+}
 
 describe('WhatToFish', () => {
   it('renders a heading', () => {
+    render(<WhatToFish />)
+
+    const heading = screen.getByRole('heading', {
+      name: /What to Fish/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders for spring', async () => {
+    date.setMonth('5')
+    // eslint-disable-next-line
+    jest.useFakeTimers().setSystemTime(date)
+    render(<WhatToFish />)
+
+    const heading = screen.getByRole('heading', {
+      name: /What to Fish/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders for summer', async () => {
+    date.setMonth('8')
+    // eslint-disable-next-line
+    jest.useFakeTimers().setSystemTime(date)
+    render(<WhatToFish />)
+
+    const heading = screen.getByRole('heading', {
+      name: /What to Fish/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders for fall', async () => {
+    date.setMonth('11')
+    // eslint-disable-next-line
+    jest.useFakeTimers().setSystemTime(date)
+    render(<WhatToFish />)
+
+    const heading = screen.getByRole('heading', {
+      name: /What to Fish/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders for winter', async () => {
+    date.setMonth('2')
+    // eslint-disable-next-line
+    jest.useFakeTimers().setSystemTime(date)
+    render(<WhatToFish />)
+
+    const heading = screen.getByRole('heading', {
+      name: /What to Fish/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders for warm water', async () => {
+    weatherData.current.feelslike_f = 100
+
+    render(<WhatToFish />)
+
+    const heading = screen.getByRole('heading', {
+      name: /What to Fish/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders for cold water', async () => {
+    weatherData.current.feelslike_f = 32
+
     render(<WhatToFish />)
 
     const heading = screen.getByRole('heading', {
