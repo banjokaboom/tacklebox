405f6d35feccc6982b2a14ee8ad6a7a9751f040e
Andrew Augusto <augustoandrew@gmail.com>
2023-06-27 13:54:43 -0400

    cleared <p> warning from logs, attempted to resolve the audit error

--- app/components/content.tsx
+++ app/components/content.tsx
@@ -3,7 +3,7 @@ export default function ContentSection({ title, content }) {
     <div>
       <h2 className="text-2xl pb-8 pt-8">{title}</h2>
       <div className="border border-slate-50 bg-slate-700 p-4 rounded-md">
-        <p>{content}</p>
+        <div>{content}</div>
       </div>
     </div>
   )
