bc8c70f9ac0df0a8b40e40070caeede095e416ed
Andrew Augusto <augustoandrew@gmail.com>
2023-06-29 14:39:11 -0400

    added adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7012167562751887"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>
         <Header></Header>
         <main className="p-12 lg:p-24 lg:pt-16 lg:pb-16 bg-cyan-700 mx-auto">
