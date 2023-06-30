a674f623707117297a422a05845f9ea9085517ef
Andrew Augusto <augustoandrew@gmail.com>
2023-06-30 11:13:53 -0400

    added changelogs

---
+++ __tests__/app/pages/changelog/page.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from '@jest/globals'
+import Changelog from '../../../../app/pages/changelog/page'
+import '@testing-library/jest-dom'
+
+describe('Changelog', () => {
+  it('renders a heading', () => {
+    render(<Changelog />)
+
+    const heading = screen.getByRole('heading', {
+      name: /Changelog/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
