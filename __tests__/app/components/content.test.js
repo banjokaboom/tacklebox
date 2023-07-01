72cd1718d60aa0ac99a49f20f97eaaad64ec7b5d
Andrew Augusto <augustoandrew@gmail.com>
2023-07-01 00:56:48 -0400

    expanded content test

--- __tests__/app/components/content.test.js
+++ __tests__/app/components/content.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import { describe, it, expect } from '@jest/globals'
 import Content from '../../../app/components/content'
 import '@testing-library/jest-dom'
@@ -6,6 +6,30 @@ import '@testing-library/jest-dom'
 describe('Content', () => {
   it('renders component', () => {
     render(<Content title="Test Title" content="Test Content" />)
+
+    const title = screen.getByText('Test Title')
+    const content = screen.getByText('Test Content')
+
+    expect(title).toBeInTheDocument()
+    expect(content).toBeInTheDocument()
+  })
+
+  it('expands when title is clicked', () => {
+    render(<Content title="Test Title" content="Test Content" />)
+
+    const button = screen.getByText('Test Title')
+    const content = screen.getByText('Test Content')
+
+    expect(button).toBeInTheDocument()
+    expect(content.parentNode.className.includes('hidden')).toBeTruthy()
+
+    fireEvent.click(button)
+
+    expect(content.parentNode.className.includes('hidden')).toBeFalsy()
+  })
+
+  it('expands when subtitle is clicked when there is no title', () => {
+    render(<Content subtitle="Test Title" content="Test Content" />)
 
     const title = screen.getByText('Test Title')
     const content = screen.getByText('Test Content')
