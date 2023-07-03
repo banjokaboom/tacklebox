732dc66ce059dbd10f454c5e19dc9de77f033bf8
Andrew Augusto <augustoandrew@gmail.com>
2023-07-03 17:21:42 -0400

    attempting to resolve the 404 issue in prod

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,16 +5,23 @@ import { usePathname, redirect } from 'next/navigation'
 export default function Home() {
   const pathname = usePathname() || ''
 
-  console.log(pathname)
-
-  if (pathname.includes('pages')) {
-    redirect(pathname.replace('pages/', ''))
+  try {
+    if (pathname.includes('pages')) {
+      redirect(pathname.replace('pages/', ''))
+    }
+  } catch (e) {
+    console.error(e)
   }
 
   return (
     <div className="flex flex-col items-center justify-between">
       <div className="max-w-5xl w-full">
         <h1 className="text-3xl mb-4">404 Not Found</h1>
+        <hr className="mb-4" />
+        <p>
+          The fish you are looking for either doesn&apos;t exist or moved.
+          Double check your sonar settings or click one of the links below.
+        </p>
       </div>
     </div>
   )
