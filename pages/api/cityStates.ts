fcb20880b20aa272ba09550b3da9ec976e59ee22
Andrew Augusto <augustoandrew@gmail.com>
2023-07-10 15:43:26 -0400

    implemented retryCount on sql calls

--- pages/api/cityStates.ts
+++ pages/api/cityStates.ts
@@ -2,16 +2,25 @@ import { sql } from '@vercel/postgres'
 import CityState from '@/app/classes/CityState'
 
 export default async function handler(req: any, res: any) {
-  const result = await sql`SELECT * from citystates`
-  let cityStates: CityState[] = []
+  let retryCount = 0
+  try {
+    const result = await sql`SELECT * from citystates`
+    let cityStates: CityState[] = []
 
-  result.rows.map((cs) => {
-    cityStates.push({
-      state: cs.state,
-      capital: cs.capital,
-      location: cs.location,
+    result.rows.map((cs) => {
+      cityStates.push({
+        state: cs.state,
+        capital: cs.capital,
+        location: cs.location,
+      })
     })
-  })
 
-  res.json({ cityStates })
+    res.json({ cityStates })
+  } catch (error) {
+    console.error(error)
+    if (retryCount < 3) {
+      retryCount++
+      handler(req, res)
+    }
+  }
 }
--- pages/api/tackle.ts
+++ pages/api/tackle.ts
@@ -2,28 +2,37 @@ import { sql } from '@vercel/postgres'
 import Tackle from '@/app/classes/Tackle'
 
 export default async function handler(req: any, res: any) {
-  let result: any = null
+  let retryCount = 0
+  try {
+    let result: any = null
 
-  if (req.query.species) {
-    result =
-      await sql`SELECT * from tackle where ${req.query.species} = ANY(species)`
-  } else {
-    result = await sql`SELECT * from tackle`
-  }
+    if (req.query.species) {
+      result =
+        await sql`SELECT * from tackle where ${req.query.species} = ANY(species)`
+    } else {
+      result = await sql`SELECT * from tackle`
+    }
 
-  let tackle: Tackle[] = []
+    let tackle: Tackle[] = []
 
-  result.rows.map((t: any) => {
-    tackle.push({
-      name: t.name,
-      confidence: t.confidence,
-      species: t.species,
-      waterTemp: t.watertemp,
-      depth: t.depth,
-      type: t.type,
-      tip: t.tip,
+    result.rows.map((t: any) => {
+      tackle.push({
+        name: t.name,
+        confidence: t.confidence,
+        species: t.species,
+        waterTemp: t.watertemp,
+        depth: t.depth,
+        type: t.type,
+        tip: t.tip,
+      })
     })
-  })
 
-  res.json({ tackle })
+    res.json({ tackle })
+  } catch (error) {
+    console.error(error)
+    if (retryCount < 3) {
+      retryCount++
+      handler(req, res)
+    }
+  }
 }
